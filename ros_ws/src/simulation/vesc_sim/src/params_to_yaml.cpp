c828066496f0f70ecf31d838b74935caeab945a4
Jan-Henrik Bruhn <hi@jhbruhn.de>
2020-01-20 19:59:19 +0100

    Emulate old yaml library behaviour

--- ros_ws/src/simulation/vesc_sim/src/params_to_yaml.cpp
+++ ros_ws/src/simulation/vesc_sim/src/params_to_yaml.cpp
@@ -2,17 +2,25 @@
 #include "yaml-cpp/yaml.h"
 #include <fstream>
 #include <ros/package.h>
+#include <limits.h>
 
 int main(int argc, char** argv)
 {
-    YAML::Node node;
-    node["speed_to_erpm_gain"] = car_config::SPEED_TO_ERPM;
-    node["speed_to_erpm_offset"] = 0;
-    node["steering_angle_to_servo_gain"] = car_config::STEERING_TO_SERVO_GAIN;
-    node["steering_angle_to_servo_offset"] = car_config::STEERING_TO_SERVO_OFFSET;
-    node["wheelbase"] = car_config::WHEELBASE;
+    YAML::Emitter out;
+    out << YAML::BeginMap;
+    out << YAML::Key << "speed_to_erpm_gain";
+    out << YAML::Value << YAML::Precision(15) << car_config::SPEED_TO_ERPM;
+    out << YAML::Key << "speed_to_erpm_offset";
+    out << YAML::Value << 0;
+    out << YAML::Key << "steering_angle_to_servo_gain";
+    out << YAML::Value << YAML::Precision(15) << car_config::STEERING_TO_SERVO_GAIN;
+    out << YAML::Key << "steering_angle_to_servo_offset";
+    out << YAML::Value << YAML::Precision(15) << car_config::STEERING_TO_SERVO_OFFSET;
+    out << YAML::Key << "wheelbase";
+    out << YAML::Value << YAML::Precision(15) << car_config::WHEELBASE;
+    out << YAML::EndMap;
 
     std::string package_path = ros::package::getPath("vesc_sim");
     std::ofstream fout(package_path + "/config/car_config.yaml");
-    fout << node;
+    fout << out.c_str();
 }
