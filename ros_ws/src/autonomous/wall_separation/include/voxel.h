6cfe2041da9a532db39c432b540b9e979e25abb0
Jan Peter Meyer <jan-peter.meyer@tu-dortmund.de>
2020-01-29 22:42:05 +0100

    wall_seperation um senden von Voxels erweitert. Einschränkung Koordinaten von double -> float. voxel_classifier erstellt und liest Daten der Pointcloud der Voxels ein. Launch File an neuen Node angepasst

--- ros_ws/src/autonomous/wall_separation/include/voxel.h
+++ ros_ws/src/autonomous/wall_separation/include/voxel.h
@@ -1,48 +1,45 @@
 #pragma once
 
 #include <inttypes.h>
+//#include <sensor_msgs/PointField.h>
 
 #define VOXEL_HISTORY_LENGTH 20
 
-class Voxel
-{
-    public:
-    double x;
-    double y;
-    uint32_t get_score()
-    {
-        if (this->score_valid)
-            return this->score;
-        // average over the last scores != 0.
-        double score = 0;
-        double count = 0;
-        for (int i = 0; i < VOXEL_HISTORY_LENGTH; i++)
-        {
-            if (this->scores[i] != 0)
-            {
-                score += this->scores[i];
-                count++;
-            }
-        }
-        this->score_valid = true;
-        return this->score = (uint32_t)(score / count);
-    }
+class Voxel {
+public:
+  float x;
+  float y;
 
-    void start_new_episode()
-    {
-        // Move all array elements to the right, shifting out the last element.
-        memmove(this->scores + 1 * sizeof(uint32_t), this->scores, VOXEL_HISTORY_LENGTH - 1);
-        this->scores[0] = 0;
-        this->score_valid = false;
+  uint32_t get_score() {
+    if (this->score_valid)
+      return this->score;
+    // average over the last scores != 0.
+    double score = 0;
+    double count = 0;
+    for (int i = 0; i < VOXEL_HISTORY_LENGTH; i++) {
+      if (this->scores[i] != 0) {
+        score += this->scores[i];
+        count++;
+      }
     }
+    this->score_valid = true;
+    return this->score = (uint32_t)(score / count);
+  }
 
-    void increment_score()
-    {
-        this->scores[0]++;
-    }
+  void start_new_episode() {
+    // Move all array elements to the right, shifting out the last element.
+    memmove(this->scores + 1 * sizeof(uint32_t), this->scores,
+            VOXEL_HISTORY_LENGTH - 1);
+    this->scores[0] = 0;
+    this->score_valid = false;
+  }
 
-    private:
-    bool score_valid;
-    uint32_t score;
-    uint32_t scores[VOXEL_HISTORY_LENGTH];
+  void increment_score() { this->scores[0]++; }
+
+  void setScore(uint32_t pScore) { this->score = pScore; }
+
+private:
+  bool score_valid;
+  uint32_t score;
+  uint32_t scores[VOXEL_HISTORY_LENGTH];
 };
