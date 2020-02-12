b76a8b5abba1aa2c8e395c359cba920b4f41742f
Marvin Langenkaemper <marvin.langenkaemper@tu-dortmund.de>
2020-02-12 16:08:26 +0100

    current time and voxel array to database

--- ros_ws/src/autonomous/store_training_data/script/store_training_data.py
+++ ros_ws/src/autonomous/store_training_data/script/store_training_data.py
@@ -4,7 +4,6 @@ import sys
 import numpy
 import rospy
 import datetime
-#import timezone
 import psycopg2
 from sensor_msgs.msg import LaserScan
 from drive_msgs.msg import drive_param
@@ -19,7 +18,6 @@ TOPIC_DRIVE_PARAMETERS = "/input/drive_param/autonomous"
 
 voxel_resolution = 0.2
 
-connection = None
 
 def connect_to_database():
     try:
@@ -30,6 +28,7 @@ def connect_to_database():
                                     port = "5432",
                                     database = "testdb")
 
+        global cursor
         cursor = connection.cursor()
         # Print PostgreSQL Connection properties
         print ( connection.get_dsn_parameters(),"\n")
@@ -43,29 +42,22 @@ def connect_to_database():
         print ("Error while connecting to PostgreSQL", error)
     
 def write_entry_to_db(current_time,current_speed,voxel,new_velocity,new_angle):
-    cursor = connection.cursor()
+    current_time_formated = current_time.strftime("'%m-%d-%Y %H:%M:%S.%f'") #"'01-01-2000 00:00:00.000'"
+    numpy.set_printoptions(threshold=sys.maxsize)
+    voxel_string = numpy.array2string(voxel, separator=',')
+    sector_number=0
+    sector_time=0
 
-    #current_time_formated = current_time.astimezone(timezone.utc).strftime("%m-%d-%Y %H:%M:%S.%f")
-
-    current_time_formated = "'01-01-2000 00:00:00-00'"
-
-    voxel_string= "'{"
+    voxel_string=voxel_string.replace('[','{').replace(']','}').replace('False','f').replace('True','t')
+    voxel_string="'"+voxel_string+"'"
 
 
-    #TODO noch fuer numpy array umschreiben
-    for i in voxel:
-        voxel_string = voxel_string + "{"
-        for j in i:
-            voxel_string = voxel_string +  str(j) + ","
-        voxel_string = voxel_string[:-1] + "}" + ","
-    voxel_string = voxel_string[:-1] + "}'"
-            
+    values_string = "VALUES("+current_time_formated+", "+str(current_speed)+","+voxel_string+", "+str(new_velocity)+","+str(new_angle)+","+str(sector_time)+","+str(sector_number)+")"
 
-    values_string = "VALUES("+current_time_formated+", "+str(current_speed)+","+voxel_string+", "+str(new_velocity)+","+str(new_angle)+")"
-
-    cursor.execute("INSERT INTO public.training_data (ts,speed,voxel,calc_velocity,calc_angle) "+ values_string+";")
-    connection.commit()
-    cursor.close()
+    try:
+        cursor.execute("INSERT INTO public.training_data (ts,speed,voxel,calc_velocity,calc_angle,sector_time,sector_number) "+ values_string+";")
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while storing training data in PostgreSQL", error)
 
 def laser_callback(scan_message):
     last_scan_message = scan_message
@@ -88,19 +80,15 @@ def voxel_callback(voxel_message):
         y= p[1]
         z= p[2]
         score= p[3]
-        print " x : %f  y: %f  z: %f  score: %f" %(x,y,z,score)
         x_index = x/voxel_resolution +50
         y_index = y/voxel_resolution +50
         if (abs(x_index)<=100 and abs(y_index)<=100):
             voxel[int(x_index),int(y_index)] = 1        #TODO bisher nicht wirklich effizient
 
-    numpy.set_printoptions(threshold=sys.maxsize)
-    print(voxel)
-
     new_velocity = 0
     new_angle = 0
 
-    #write_entry_to_db(current_time, current_speed,voxel,new_velocity,new_angle)
+    write_entry_to_db(current_time, current_speed,voxel,new_velocity,new_angle)
 
 
 def drive_callback(drive_message):
@@ -119,5 +107,7 @@ while not rospy.is_shutdown():
     rospy.spin()
 
 if(connection):
+    connection.commit()
+    cursor.close()
     connection.close()
     print("PostgreSQL connection is closed")
