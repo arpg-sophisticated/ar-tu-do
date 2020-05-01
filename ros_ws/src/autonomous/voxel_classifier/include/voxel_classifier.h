168c1505b201feb6f965beea821688800c16caa9
Jan-Henrik Bruhn <hi@jhbruhn.de>
2020-05-01 15:09:17 +0200

    Start clean up of voxel_classifier

--- ros_ws/src/autonomous/voxel_classifier/include/voxel_classifier.h
+++ ros_ws/src/autonomous/voxel_classifier/include/voxel_classifier.h
@@ -1,7 +1,6 @@
 #pragma once
+
 #include "dbscan.h"
-#include "rviz_geometry_publisher.h"
-#include "voxel.h"
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/point_cloud.h>
@@ -11,27 +10,22 @@
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
-constexpr const char *TOPIC_VOXEL_ = "/scan/voxels";
-constexpr const char *TOPIC_CLUSTER_ = "/scan/cluster";
-constexpr const char *TOPIC_VISUALIZATION_Cluster =
-    "/wall_separation_visualization_cluster";
-constexpr const char *LIDAR_FRAME = "laser";
+constexpr const char* TOPIC_VOXEL_ = "/scan/voxels";
+constexpr const char* TOPIC_CLUSTER_ = "/scan/cluster";
+constexpr const char* TOPIC_VISUALIZATION_Cluster = "/wall_separation_visualization_cluster";
+constexpr const char* LIDAR_FRAME = "laser";
 
-class VoxelClassifier {
-private:
-  ros::NodeHandle m_node_handle;
-  ros::Subscriber m_voxel_subscriber;
-  std::vector<Voxel> voxels;
-  RvizGeometryPublisher m_debug_geometry;
-  ros::Publisher m_marker_publisher;
-  ros::Publisher m_cluster_publisher;
-  std::string m_frame;
+class VoxelClassifier
+{
+    private:
+    ros::NodeHandle m_node_handle;
+    ros::Subscriber m_voxel_subscriber;
+    ros::Publisher m_marker_publisher;
+    ros::Publisher m_cluster_publisher;
+    std::string m_frame;
 
-public:
-  VoxelClassifier();
-  void voxel_callback(const sensor_msgs::PointCloud2::ConstPtr &lidar);
-  void cluster_publish();
-  // std::vector<Point> transformPoints();
-  void printResults(vector<Voxel> &points, int num_points);
-  void clearRects();
+    public:
+    VoxelClassifier();
+    void voxel_callback(const sensor_msgs::PointCloud2::ConstPtr& lidar);
+    void cluster_publish(std::vector<Point_>* clusters);
 };
--- ros_ws/src/autonomous/voxel_classifier/src/voxel_classifier.cpp
+++ ros_ws/src/autonomous/voxel_classifier/src/voxel_classifier.cpp
@@ -4,87 +4,55 @@
 #define EPSILON (0.47 * 0.47)
 
 VoxelClassifier::VoxelClassifier()
-    : m_debug_geometry(m_node_handle, TOPIC_VISUALIZATION_Cluster, LIDAR_FRAME)
 {
     this->m_voxel_subscriber =
         m_node_handle.subscribe<sensor_msgs::PointCloud2>(TOPIC_VOXEL_, 1, &VoxelClassifier::voxel_callback, this);
 
     this->m_cluster_publisher = m_node_handle.advertise<pcl::PointCloud<pcl::PointXYZI>>(TOPIC_CLUSTER_, 1);
 }
-/*std::vector<Point> VoxelClassifier::transformPoints(){
-  vector<Point> r_points;
-  for (size_t i = 0; i < voxels.size(); i++)
-  {
-     Point tmp;
-     tmp.x=voxels[i].x;
-     tmp.y=voxels[i].y;
-     tmp.z=0;
-     tmp.clusterID=0;
-     tmp.clusterID=UNCLASSIFIED;
-     r_points.push_back(tmp);
-  }
-  return r_points;
-}*/
 
 void VoxelClassifier::voxel_callback(const sensor_msgs::PointCloud2::ConstPtr& voxelPointcloud)
 {
-    voxels.clear();
 
     pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloud(new pcl::PointCloud<pcl::PointXYZI>);
     pcl::fromROSMsg(*voxelPointcloud, *inputCloud);
 
     m_frame = voxelPointcloud->header.frame_id;
 
+    std::vector<Point_> dbScanPoints;
     for (size_t i = 0; i < inputCloud->size(); i++)
     {
-        Voxel tmp;
+        Point_ tmp;
         tmp.x = (*inputCloud)[i].x;
         tmp.y = (*inputCloud)[i].y;
         tmp.z = (*inputCloud)[i].z;
-        tmp.setScore((*inputCloud)[i].intensity);
         tmp.clusterID = -1;
-        voxels.push_back(tmp);
+        dbScanPoints.push_back(tmp);
     }
 
-    DBSCAN ds(MINIMUM_POINTS, EPSILON, &voxels);
+    DBSCAN ds(MINIMUM_POINTS, EPSILON, &dbScanPoints);
     ds.run();
-    // printResults(voxels, voxels.size());
-    cluster_publish();
+
+    cluster_publish(&dbScanPoints);
 }
 
-void VoxelClassifier::cluster_publish()
+void VoxelClassifier::cluster_publish(std::vector<Point_>* clusters)
 {
     pcl::PointCloud<pcl::PointXYZI>::Ptr msg(new pcl::PointCloud<pcl::PointXYZI>);
     msg->header.frame_id = m_frame;
-    msg->height = 1;
-    msg->width = voxels.size();
-    // msg->points.push_back (pcl::PointXYZ(1.0, 2.0, 3.0));
 
-    for (size_t i = 0; i < voxels.size(); i++)
+    for (size_t i = 0; i < clusters->size(); i++)
     {
         pcl::PointXYZI tmp;
-        tmp.x = voxels[i].x;
-        tmp.y = voxels[i].y;
-        tmp.z = voxels[i].z;
-        tmp.intensity = voxels[i].clusterID;
+        tmp.x = (*clusters)[i].x;
+        tmp.y = (*clusters)[i].y;
+        tmp.z = (*clusters)[i].z;
+        tmp.intensity = (*clusters)[i].clusterID;
         msg->push_back(tmp);
     }
+
     pcl_conversions::toPCL(ros::Time::now(), msg->header.stamp);
     m_cluster_publisher.publish(msg);
-}
-
-void VoxelClassifier::printResults(vector<Voxel>& points, int num_points)
-{
-    int i = 0;
-    printf("Number of points: %u\n"
-           " x     y     z     cluster_id\n"
-           "-----------------------------\n",
-           num_points);
-    while (i < num_points)
-    {
-        printf("%5.2lf %5.2lf %5.2lf: %d\n", points[i].x, points[i].y, points[i].z, points[i].clusterID);
-        ++i;
-    }
 }
 
 int main(int argc, char** argv)
