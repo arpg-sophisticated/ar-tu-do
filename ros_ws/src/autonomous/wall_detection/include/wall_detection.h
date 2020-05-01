d69a1cafb0e6a2365dfbcf08fc48279086c682d2
Jan-Henrik Bruhn <hi@jhbruhn.de>
2020-05-01 15:27:32 +0200

    Clean up code of wall_detection

--- ros_ws/src/autonomous/wall_detection/include/wall_detection.h
+++ ros_ws/src/autonomous/wall_detection/include/wall_detection.h
@@ -8,33 +8,28 @@
 #include <stdlib.h>
 #include <unordered_map>
 
-constexpr const char *TOPIC_VOXEL_ = "/scan/cluster";
-constexpr const char *TOPIC_WALLS_ = "/obstacles/walls";
-constexpr const char *TOPIC_OBSTACLES_ = "/obstacles/obstacles";
+constexpr const char* TOPIC_VOXEL_ = "/scan/cluster";
+constexpr const char* TOPIC_WALLS_ = "/obstacles/walls";
+constexpr const char* TOPIC_OBSTACLES_ = "/obstacles/obstacles";
 
-constexpr const char *LIDAR_FRAME = "laser";
 typedef pcl::PointCloud<pcl::PointXYZI> PointCloud;
 
-class WallDetection {
-private:
-  ros::NodeHandle m_node_handle;
-  ros::Subscriber m_voxel_subscriber;
-  ros::Publisher m_wall_publisher;
-  ros::Publisher m_obstacles_publisher;
-  const std::string m_frame;
-  std::vector<PointCloud> listOfClusters;
-  std::pair<int, int>
-  voxelMaximaIDs(std::unordered_map<int, std::vector<pcl::PointXYZI> *>);
-  void publishWall(std::vector<pcl::PointXYZI> *wallLeft,
-                   std::vector<pcl::PointXYZI> *wallRight);
-  void publishObstacles(
-      std::unordered_map<int, std::vector<pcl::PointXYZI> *> mapClusters,
-      std::pair<int, int> wallIDs);
-  std::string frameID;
+class WallDetection
+{
+    private:
+    ros::NodeHandle m_node_handle;
+    ros::Subscriber m_voxel_subscriber;
+    ros::Publisher m_wall_publisher;
+    ros::Publisher m_obstacles_publisher;
+    const std::string m_frame;
+    std::vector<PointCloud> listOfClusters;
+    std::pair<int, int> determineWallIDs(std::unordered_map<int, std::vector<pcl::PointXYZI>*>, float radius);
+    void publishWall(std::vector<pcl::PointXYZI>* wallLeft, std::vector<pcl::PointXYZI>* wallRight);
+    void publishObstacles(std::unordered_map<int, std::vector<pcl::PointXYZI>*> mapClusters,
+                          std::pair<int, int> wallIDs);
+    std::string frameID;
 
-public:
-  WallDetection();
-  void wallDetection_callback(
-      const pcl::PointCloud<pcl::PointXYZI>::ConstPtr &inputVoxels);
-  PointCloud pointCloudOfID(const PointCloud::ConstPtr &inputVoxels, int ID);
+    public:
+    WallDetection();
+    void wallDetection_callback(const pcl::PointCloud<pcl::PointXYZI>::ConstPtr& inputVoxels);
 };
--- ros_ws/src/autonomous/wall_detection/src/wall_detection.cpp
+++ ros_ws/src/autonomous/wall_detection/src/wall_detection.cpp
@@ -1,178 +1,135 @@
 #include "wall_detection.h"
 
-WallDetection::WallDetection() {
-  this->m_voxel_subscriber =
-      m_node_handle.subscribe<pcl::PointCloud<pcl::PointXYZI>>(
-          TOPIC_VOXEL_, 1, &WallDetection::wallDetection_callback, this);
+WallDetection::WallDetection()
+{
+    this->m_voxel_subscriber =
+        m_node_handle.subscribe<pcl::PointCloud<pcl::PointXYZI>>(TOPIC_VOXEL_, 1,
+                                                                 &WallDetection::wallDetection_callback, this);
 
-  this->m_wall_publisher =
-      m_node_handle.advertise<pcl::PointCloud<pcl::PointXYZI>>(TOPIC_WALLS_, 1);
+    this->m_wall_publisher = m_node_handle.advertise<pcl::PointCloud<pcl::PointXYZI>>(TOPIC_WALLS_, 1);
 
-  this->m_obstacles_publisher =
-      m_node_handle.advertise<pcl::PointCloud<pcl::PointXYZI>>(TOPIC_OBSTACLES_,
-                                                               1);
+    this->m_obstacles_publisher = m_node_handle.advertise<pcl::PointCloud<pcl::PointXYZI>>(TOPIC_OBSTACLES_, 1);
 }
 
-void WallDetection::wallDetection_callback(
-    const pcl::PointCloud<pcl::PointXYZI>::ConstPtr &inputVoxels) {
+void WallDetection::wallDetection_callback(const pcl::PointCloud<pcl::PointXYZI>::ConstPtr& inputVoxels)
+{
 
-  frameID = inputVoxels->header.frame_id;
+    frameID = inputVoxels->header.frame_id;
 
-  printf("Cloasdasddaud: width = %d, height = %d\n", inputVoxels->width,
-         inputVoxels->height);
-
-  // std::map<int,std::vector<pcl::PointXYZ>> clustersUsed;
-
-  std::unordered_map<int, std::vector<pcl::PointXYZI> *> clustersUsed;
-
-  for (size_t i = 0; i < inputVoxels->points.size(); i++) {
-    // if (clustersUsed.find(inputVoxels->points[i].z)!= clustersUsed.end())
-    if (clustersUsed.count(inputVoxels->points[i].intensity) > 0) {
-
-      clustersUsed[inputVoxels->points[i].intensity]->push_back(
-          inputVoxels->points[i]);
-      // std::cout << "Der Liste wurde ein neues Element hinzugefügt und beträgt
-      // "
-      //              "nun die Läne: "
-      //           << clustersUsed[inputVoxels->points[i].z]->size() << "\n";
-
-    } else {
-      // std::cout << "Erster Add der Liste\n";
-      std::vector<pcl::PointXYZI> *tmp = new std::vector<pcl::PointXYZI>();
-      tmp->push_back(inputVoxels->points[i]);
-      clustersUsed.insert({inputVoxels->points[i].intensity, tmp});
-
-      std::cout << clustersUsed.size() << "\n";
+    std::unordered_map<int, std::vector<pcl::PointXYZI>*> clustersUsed;
+    // map cluster ids in intensity to map with cluster id as key and pointvector as value
+    for (size_t i = 0; i < inputVoxels->points.size(); i++)
+    {
+        if (clustersUsed.count(inputVoxels->points[i].intensity) > 0)
+        {
+            clustersUsed[inputVoxels->points[i].intensity]->push_back(inputVoxels->points[i]);
+        }
+        else
+        {
+            std::vector<pcl::PointXYZI>* tmp = new std::vector<pcl::PointXYZI>();
+            tmp->push_back(inputVoxels->points[i]);
+            clustersUsed.insert({ inputVoxels->points[i].intensity, tmp });
+        }
     }
 
-    // std::cout << "Punkt Nr. " << i << " --------- " <<
-    // inputVoxels->points[i].x
-    //           << "|" << inputVoxels->points[i].y << "|"
-    //           << inputVoxels->points[i].z << "\n";
-  }
+    // determine maximum left and right clusters in a radius
+    auto test = determineWallIDs(clustersUsed, 3.0); // these ids are the walls
 
-  auto test = voxelMaximaIDs(clustersUsed);
+    // publish only the clusters with ids equal to the walls, but only if the id is > 0
+    if (test.first >= 0 && test.second >= 0)
+        publishWall(clustersUsed[test.first], clustersUsed[test.second]);
 
-  std::cout << "Hier unsere Lösung: " << test.first << "|" << test.second
-            << "\n";
+    // publish all other clusters
+    publishObstacles(clustersUsed, test);
 
-  std::cout << "VOR Publish Wall";
-
-  publishWall(clustersUsed[test.first], clustersUsed[test.second]);
-  publishObstacles(clustersUsed, test);
-
-  std::cout << "Nach Publish Wall";
-
-  // this->m_debug_geometry.drawVoxels(0, (clustersUsed[test.first]*) ,
-  // voxelResolution,
-  //                                   voxelResolution, 1 / 10.0f);
-  // for (size_t i = 0; i < clustersUsed.at(2).size(); i++)
-  // {
-  //   std::cout << clustersUsed.at(2)[0].x << "|" << clustersUsed.at(2)[0].y <<
-  //   "|" << clustersUsed.at(2)[0].z <<"\n" ;
-  // }
-
-  // BOOST_FOREACH (const pcl::PointXYZ& pt, inputVoxels->points)
-  //    printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
+    // clean up
+    for (auto itr = clustersUsed.begin(); itr != clustersUsed.end(); ++itr)
+        delete itr->second;
 }
 
-void WallDetection::publishObstacles(
-    std::unordered_map<int, std::vector<pcl::PointXYZI> *> mapClusters,
-    std::pair<int, int> wallIDs) {
-  pcl::PointCloud<pcl::PointXYZI>::Ptr msg(new pcl::PointCloud<pcl::PointXYZI>);
+void WallDetection::publishObstacles(std::unordered_map<int, std::vector<pcl::PointXYZI>*> mapClusters,
+                                     std::pair<int, int> wallIDs)
+{
+    pcl::PointCloud<pcl::PointXYZI>::Ptr msg(new pcl::PointCloud<pcl::PointXYZI>);
 
-  msg->header.frame_id = frameID;
+    msg->header.frame_id = frameID;
 
-  for (auto itr = mapClusters.begin(); itr != mapClusters.end(); ++itr) {
-    if (itr->first != wallIDs.first && itr->first != wallIDs.second) {
-      for (size_t i = 0; i < itr->second->size(); i++) {
-        msg->push_back((*itr->second)[i]);
-      }
+    for (auto itr = mapClusters.begin(); itr != mapClusters.end(); ++itr)
+    {
+        if (itr->first != wallIDs.first && itr->first != wallIDs.second)
+        {
+            for (size_t i = 0; i < itr->second->size(); i++)
+            {
+                msg->push_back((*itr->second)[i]);
+            }
+        }
+    }
+    pcl_conversions::toPCL(ros::Time::now(), msg->header.stamp);
+    m_obstacles_publisher.publish(msg);
+}
+
+void WallDetection::publishWall(std::vector<pcl::PointXYZI>* wallLeft, std::vector<pcl::PointXYZI>* wallRight)
+{
+    pcl::PointCloud<pcl::PointXYZI>::Ptr msg(new pcl::PointCloud<pcl::PointXYZI>);
+    msg->header.frame_id = frameID;
+
+    for (size_t i = 0; i < wallLeft->size(); i++)
+    {
+        pcl::PointXYZI tmp;
+        tmp.x = (*wallLeft)[i].x;
+        tmp.y = (*wallLeft)[i].y;
+        tmp.z = (*wallLeft)[i].z;
+        tmp.intensity = 1;
+
+        msg->push_back(tmp);
     }
 
-    // for (auto itrVector = itr->second->begin(); itrVector !=
-    // itr->second->end();
-    //      ++itrVector) {
-    //   if ((itrVector->y > maxLeft) && (itrVector->x <= Schwellwert) &&
-    //       (itrVector->x >= -Schwellwert)) {
-    //     maxLeft = itrVector->x;
-    //     maxLeftID = itrVector->z;
-    //   }
-    //   if ((itrVector->y < maxRight) && (itrVector->x <= Schwellwert) &&
-    //       (itrVector->x >= -Schwellwert)) {
-    //     maxRight = itrVector->x;
-    //     maxRightID = itrVector->z;
-    //   }
-    // }
-  }
-  pcl_conversions::toPCL(ros::Time::now(), msg->header.stamp);
-  m_obstacles_publisher.publish(msg);
-}
+    for (size_t i = 0; i < wallRight->size(); i++)
+    {
+        pcl::PointXYZI tmp;
+        tmp.x = (*wallRight)[i].x;
+        tmp.y = (*wallRight)[i].y;
+        tmp.z = (*wallRight)[i].z;
+        tmp.intensity = 0;
 
-void WallDetection::publishWall(std::vector<pcl::PointXYZI> *wallLeft,
-                                std::vector<pcl::PointXYZI> *wallRight) {
-  pcl::PointCloud<pcl::PointXYZI>::Ptr msg(new pcl::PointCloud<pcl::PointXYZI>);
-  msg->header.frame_id = frameID;
-  // msg->height = 1;
-  // msg->width = wallLeft->size() + wallRight->size();
-  // msg->points.push_back (pcl::PointXYZ(1.0, 2.0, 3.0));
-
-  for (size_t i = 0; i < wallLeft->size(); i++) {
-    pcl::PointXYZI tmp;
-    tmp.x = (*wallLeft)[i].x;
-    tmp.y = (*wallLeft)[i].y;
-    tmp.z = (*wallLeft)[i].z;
-    tmp.intensity = 1;
-
-    msg->points.push_back(tmp);
-
-    // pcl::PointXYZI(wallLeft[i]., voxels[i].y, voxels[i].clusterID));
-  }
-  for (size_t i = 0; i < wallRight->size(); i++) {
-    pcl::PointXYZI tmp;
-    tmp.x = (*wallRight)[i].x;
-    tmp.y = (*wallRight)[i].y;
-    tmp.z = (*wallRight)[i].z;
-    tmp.intensity = 0;
-
-    msg->push_back(tmp);
-
-    // pcl::PointXYZI(wallLeft[i]., voxels[i].y, voxels[i].clusterID));
-  }
-  pcl_conversions::toPCL(ros::Time::now(), msg->header.stamp);
-  m_wall_publisher.publish(msg);
-}
-
-std::pair<int, int> WallDetection::voxelMaximaIDs(
-    std::unordered_map<int, std::vector<pcl::PointXYZI> *> mapToCheck) {
-  float maxLeft = 0;
-  float maxRight = 0;
-  int maxLeftID = -1;
-  int maxRightID = -1;
-
-  int Schwellwert = 3;
-
-  for (auto itr = mapToCheck.begin(); itr != mapToCheck.end(); ++itr) {
-    for (auto itrVector = itr->second->begin(); itrVector != itr->second->end();
-         ++itrVector) {
-      if ((itrVector->y > maxLeft) && (itrVector->x <= Schwellwert) &&
-          (itrVector->x >= -Schwellwert)) {
-        maxLeft = itrVector->x;
-        maxLeftID = itrVector->intensity;
-      }
-      if ((itrVector->y < maxRight) && (itrVector->x <= Schwellwert) &&
-          (itrVector->x >= -Schwellwert)) {
-        maxRight = itrVector->x;
-        maxRightID = itrVector->intensity;
-      }
+        msg->push_back(tmp);
     }
-  }
-  return std::pair<int, int>(maxLeftID, maxRightID);
+
+    pcl_conversions::toPCL(ros::Time::now(), msg->header.stamp);
+    m_wall_publisher.publish(msg);
 }
 
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "wall_detection");
-  WallDetection wallDetection;
-  ros::spin();
-  return EXIT_SUCCESS;
+std::pair<int, int> WallDetection::determineWallIDs(std::unordered_map<int, std::vector<pcl::PointXYZI>*> mapToCheck,
+                                                    float radius)
+{
+    float maxLeft = 0;
+    float maxRight = 0;
+    int maxLeftID = -1;
+    int maxRightID = -1;
+
+    for (auto itr = mapToCheck.begin(); itr != mapToCheck.end(); ++itr)
+    {
+        for (auto itrVector = itr->second->begin(); itrVector != itr->second->end(); ++itrVector)
+        {
+            if ((itrVector->y > maxLeft) && (itrVector->x <= radius) && (itrVector->x >= -radius))
+            {
+                maxLeft = itrVector->x;
+                maxLeftID = itrVector->intensity;
+            }
+            if ((itrVector->y < maxRight) && (itrVector->x <= radius) && (itrVector->x >= -radius))
+            {
+                maxRight = itrVector->x;
+                maxRightID = itrVector->intensity;
+            }
+        }
+    }
+    return std::pair<int, int>(maxLeftID, maxRightID);
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "wall_detection");
+    WallDetection wallDetection;
+    ros::spin();
+    return EXIT_SUCCESS;
 }
