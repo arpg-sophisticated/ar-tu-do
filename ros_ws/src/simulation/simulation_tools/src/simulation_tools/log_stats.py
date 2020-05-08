77b6bb54f839d000f3b6fc835ab428594d958e06
Marvin Langenkaemper <marvin.langenkaemper@tu-dortmund.de>
2020-05-08 18:27:35 +0200

    added state telemetry to launch file self2global

--- ros_ws/src/simulation/simulation_tools/src/simulation_tools/log_stats.py
+++ ros_ws/src/simulation/simulation_tools/src/simulation_tools/log_stats.py
@@ -5,48 +5,66 @@ import sys
 from simulation_tools.track import track
 from simulation_tools.reset_car import Point
 from drive_msgs.msg import drive_param,gazebo_state_telemetry
+from gazebo_msgs.msg import ModelStates
 
-from topics import TOPIC_GAZEBO_MODEL_STATE, TOPIC_GAZEBO_STATE_TELEMETRY,TOPIC_DRIVE_PARAMETERS
-
+TOPIC_DRIVE_PARAMETERS = "/input/drive_param/autonomous"
+TOPIC_GAZEBO_MODEL_STATE = "/gazebo/model_states"
+TOPIC_GAZEBO_STATE_TELEMETRY = "/gazebo/state_telemetry"
 
 def __init__(self):
-    self.last_drive_message = None
-    self.current_speed = 0
-    self.car_position = None
-    self.car_orientation = 0
-    self.track_position = 0
+    global last_drive_message
+    global current_speed
+    global car_position
+    global car_orientation
+    global track_position
+    last_drive_message = None
+    current_speed = 0
+    car_position = None
+    car_orientation = 0
+    track_position = 0
 
-def drive_param_callback(self,message):
-    self.last_drive_message = message
+def drive_param_callback(message):
+    global last_drive_message 
+    last_drive_message = message
 
  
-def speed_callback(self,speed_message):
-    self.current_speed = speed_message.wheel_speed
-    self.log_message()
+def speed_callback(speed_message):
+    print("speed")
+    global current_speed
+    current_speed = speed_message.wheel_speed
+    log_message()
 
-def on_model_state_callback(self, message):
+def on_model_state_callback(message):
     if len(message.pose) < 2:
         return
-    self.car_position = Point(
+    global car_position
+    global car_orientation
+    global track_position
+    car_position = Point(
         message.pose[1].position.x,
         message.pose[1].position.y)
-    self.car_orientation = message.pose[1].orientation
-    self.track_position = track.localize(self.car_position)
+    car_orientation = message.pose[1].orientation
+    track_position = track.localize(car_position)
 
-def log_message(self):
+def log_message():
     velocity = 0
     angle = 0
-    if(self.last_drive_message is not None):
-        velocity = self.last_drive_message.velocity
-        angle = self.last_drive_message.angle
+    global last_drive_message
+    global track_position
+    global car_position
+    global current_speed
+    if(last_drive_message is not None):
+        velocity = last_drive_message.velocity
+        angle = last_drive_message.angle
     time = rospy.get_time()
-    with open("..\..\..\test.txt", "a") as myfile:
-        myfile.write(str(time)+", "+str(self.track_position)+", "+str(self.car_position)+", "+str(velocity)+", "+str(angle)+", "+str(self.current_speed))
-
-
-
+    print("log stats")
+    with open("test.txt", "a") as myfile:
+        myfile.write(str(time)+", "+str(track_position)+", "+str(car_position)+", "+str(velocity)+", "+str(angle)+", "+str(current_speed))
 
 rospy.init_node('log_stats', anonymous=True)
-rospy.Subscriber(TOPIC_GAZEBO_MODEL_STATE, ModelStates, self.on_model_state_callback)
-rospy.Subscriber(TOPIC_GAZEBO_STATE_TELEMETRY, gazebo_state_telemetry, self.speed_callback)
-rospy.Subscriber(TOPIC_DRIVE_PARAMETERS, drive_param, self.drive_param_callback)
+rospy.Subscriber(TOPIC_GAZEBO_MODEL_STATE, ModelStates, on_model_state_callback)
+rospy.Subscriber(TOPIC_GAZEBO_STATE_TELEMETRY, gazebo_state_telemetry, speed_callback)
+rospy.Subscriber(TOPIC_DRIVE_PARAMETERS, drive_param, drive_param_callback)
+
+while not rospy.is_shutdown():
+    rospy.spin()
