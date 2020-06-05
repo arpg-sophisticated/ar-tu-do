f26ebe52a5d0060bd031ce96fba7a7b8345a5e79
Marvin Langenkaemper <marvin.langenkaemper@tu-dortmund.de>
2020-06-05 17:24:14 +0200

    changed topic handling for drive node

--- ros_ws/src/autonomous/reinforcement_learning/scripts/drive_q_learning_time_reward.py
+++ ros_ws/src/autonomous/reinforcement_learning/scripts/drive_q_learning_time_reward.py
@@ -5,8 +5,7 @@ import os
 import rospy
 from parameters_q_learning_time_reward import *
 import torch
-from topics import TOPIC_DRIVE_PARAMETERS_WF
-TOPIC_GAZEBO_STATE_TELEMETRY = "/gazebo/state_telemetry"
+from topics import TOPIC_DRIVE_PARAMETERS_WF2RL, TOPIC_GAZEBO_STATE_TELEMETRY
 from drive_msgs.msg import drive_param
 from drive_msgs.msg import gazebo_state_telemetry
 
@@ -19,6 +18,7 @@ class QLearningDrivingNode(ReinforcementLearningNode):
     def __init__(self):
         self.policy = NeuralQEstimator().to(device)
         self.current_speed = 0
+        self.last_laser_scan_message = None
 
         try:
             self.policy.load()
@@ -31,27 +31,35 @@ class QLearningDrivingNode(ReinforcementLearningNode):
 
         ReinforcementLearningNode.__init__(self, ACTIONS, LASER_SAMPLE_COUNT)
 
-        rospy.Subscriber(TOPIC_DRIVE_PARAMETERS, drive_param, self.wallfollowing_drive_param_callback)
-        rospy.Subscriber(TOPIC_GAZEBO_STATE_TELEMETRY, gazebo_state_telemetry, self.speed_callback)
+        rospy.Subscriber(
+            TOPIC_DRIVE_PARAMETERS_WF2RL,
+            drive_param,
+            self.wallfollowing_drive_param_callback)
+        rospy.Subscriber(
+            TOPIC_GAZEBO_STATE_TELEMETRY,
+            gazebo_state_telemetry,
+            self.speed_callback)
 
      # override implemented function of reinforcement_learning_node
     def on_receive_laser_scan(self, message):
+        self.last_laser_scan_message = message
         return
 
-    def wallfollowing_drive_param_callback(self,message):
+    def wallfollowing_drive_param_callback(self, message):
         if self.policy is None:
             return
         self.lastWFmessage = message
-        if(self.current_speed ==0):
+        if(self.current_speed == 0):
             print("speed 0!!!")
 
-        state = self.convert_laser_and_speed_message_to_tensor(message.laser_scan,self.current_speed)
+        state = self.convert_laser_and_speed_message_to_tensor(
+            self.last_laser_scan_message, self.current_speed)
 
         with torch.no_grad():
             action = self.policy(state).max(0)[1].item()
-        self.perform_action(action,True)
+        self.perform_action(action, True)
 
-    def speed_callback(self,speed_message):
+    def speed_callback(self, speed_message):
         self.current_speed = speed_message.wheel_speed
         #print("Speed: "+ str(self.current_speed))
 
@@ -61,16 +69,18 @@ class QLearningDrivingNode(ReinforcementLearningNode):
             raise Exception("Invalid action: " + str(action_index))
 
         angle, velocity = self.actions[action_index]
-        print(str(angle)+", "+ str(velocity))
+        print(str(angle) + ", " + str(velocity))
         message = drive_param()
         # add WFdrivemessages
         if(addWFMessage):
-            message.angle = angle +self.lastWFmessage.angle
-            message.velocity = (velocity*SAFETY_REDUCTION) +self.lastWFmessage.velocity
+            message.angle = angle + self.lastWFmessage.angle
+            message.velocity = (velocity * SAFETY_REDUCTION) + \
+                self.lastWFmessage.velocity
         else:
             message.angle = angle
             message.velocity = velocity
         self.drive_parameters_publisher.publish(message)
+
 
 rospy.init_node('q_learning_driving', anonymous=True)
 node = QLearningDrivingNode()
