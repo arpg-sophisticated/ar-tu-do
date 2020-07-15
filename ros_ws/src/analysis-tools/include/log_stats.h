a4fb384a31604ba3d37a399de998a0d1b2324d54
Robin Thunig <robin.thunig@tu-dortmund.de>
2020-07-15 20:20:37 +0200

    add more subscriber and publisher

--- ros_ws/src/analysis-tools/include/log_stats.h
+++ ros_ws/src/analysis-tools/include/log_stats.h
@@ -1,21 +1,37 @@
 #pragma once
 
 #include <drive_msgs/drive_param.h>
+#include <drive_msgs/gazebo_state_telemetry.h>
 #include <ros/ros.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/Float64.h>
 
 constexpr const char* TOPIC_CONTROLLED_DRIVE_PARAM = "/commands/controlled_drive_param";
+constexpr const char* TOPIC_MAX_SPEED = "/speed_info/max_speed";
+constexpr const char* TOPIC_GAZEBO_STATE_TELEMETRY = "/gazebo/state_telemetry";
 
 class LogStats
 {
     private:
     ros::NodeHandle m_node_handle;
     ros::Subscriber m_controlled_drive_parameters_subscriber;
+    ros::Subscriber m_max_speed_subscriber;
+    ros::Subscriber m_gazebo_state_telemetry_subscriber;
+    ros::Publisher m_hud_speed_publisher;
 
     double m_current_speed;
+    double m_current_steering_angle;
+
+    double m_max_speed;
+
+    double m_gazebo_wheel_speed;
+    double m_gazebo_car_speed;
 
     public:
     LogStats();
 
     private:
-    void controlledDriveParametersCallback(const drive_msgs::drive_param::ConstPtr& parameters);
+    void controlledDriveParametersCallback(const drive_msgs::drive_param::ConstPtr& drive_parameters);
+    void maxSpeedCallback(const std_msgs::Float64::ConstPtr& max_speed);
+    void gazeboStateTelemetryCallback(const drive_msgs::gazebo_state_telemetry::ConstPtr& gazebo_state_telemetry);
 };
--- ros_ws/src/analysis-tools/src/log_stats.cpp
+++ ros_ws/src/analysis-tools/src/log_stats.cpp
@@ -5,11 +5,36 @@ LogStats::LogStats()
     m_controlled_drive_parameters_subscriber =
         m_node_handle.subscribe<drive_msgs::drive_param>(TOPIC_CONTROLLED_DRIVE_PARAM, 1,
                                                          &LogStats::controlledDriveParametersCallback, this);
+    m_max_speed_subscriber =
+        m_node_handle.subscribe<std_msgs::Float64>(TOPIC_MAX_SPEED, 1, &LogStats::maxSpeedCallback, this);
+    m_gazebo_state_telemetry_subscriber =
+        m_node_handle.subscribe<drive_msgs::gazebo_state_telemetry>(TOPIC_GAZEBO_STATE_TELEMETRY, 1,
+                                                                    &LogStats::gazeboStateTelemetryCallback, this);
+
+    m_hud_speed_publisher = m_node_handle.advertise<std_msgs::Float32>("hud_speed_value", 1);
 }
 
 void LogStats::controlledDriveParametersCallback(const drive_msgs::drive_param::ConstPtr& parameters)
 {
     m_current_speed = parameters->velocity;
+    m_current_steering_angle = parameters->angle;
+
+    // hud publisher example
+    std_msgs::Float32 speed_message;
+    speed_message.data = m_current_speed;
+    m_hud_speed_publisher.publish(speed_message);
+}
+
+void LogStats::maxSpeedCallback(const std_msgs::Float64::ConstPtr& max_speed)
+{
+    m_max_speed = max_speed->data;
+}
+
+void LogStats::gazeboStateTelemetryCallback(const drive_msgs::gazebo_state_telemetry::ConstPtr& gazebo_state_telemetry)
+{
+    m_gazebo_wheel_speed = gazebo_state_telemetry->wheel_speed;
+    m_gazebo_car_speed = gazebo_state_telemetry->car_speed;
+    // ... usw
 }
 
 int main(int argc, char** argv)
