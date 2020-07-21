632c87331960736ca1c1979de3c91931e2142442
Marcel Ebbrecht <marcel.ebbrecht@googlemail.com>
2020-07-21 21:12:59 +0200

    closes #122 - implemented

--- ros_ws/src/analysis-tools/include/config.h
+++ ros_ws/src/analysis-tools/include/config.h
@@ -3,4 +3,8 @@
 namespace Config
 {
     static const float USABLE_LASER_RANGE = 220;
-}
+    static const double MAX_STEERING_ANGLE = 40.0f;
+    static const double RPM_FACTOR = 1299.224f;
+    static const double LOG_INTERVAL = 0.05f;
+    static const double HUD_PRECISION = 4U;
+} // namespace Config
