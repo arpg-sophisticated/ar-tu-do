0ab5b9694fbfbd7c94e82e245a657e15a383a4e1
Robin Thunig <robin.thunig@tu-dortmund.de>
2020-07-27 11:58:04 +0200

    set dynamic_friction to 0.6

--- ros_ws/src/car_config/include/physical_properties.h
+++ ros_ws/src/car_config/include/physical_properties.h
@@ -2,6 +2,6 @@
 
 namespace PhysicalProperties
 {
-    static const float DYNAMIC_FRICTION = 0.54;
+    static const float DYNAMIC_FRICTION = 0.6;
     static const float ACCELERATION = 9.81f * DYNAMIC_FRICTION;
 }; // namespace PhysicalProperties
