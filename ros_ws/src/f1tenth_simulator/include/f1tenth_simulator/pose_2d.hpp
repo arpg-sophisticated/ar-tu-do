984b33599e9dae52b3fda551c643360049246438
Robin Thunig <robin.thunig@tu-dortmund.de>
2020-08-31 18:19:07 +0200

    add f1thenth simulator

---
+++ ros_ws/src/f1tenth_simulator/include/f1tenth_simulator/pose_2d.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace racecar_simulator
+{
+
+    struct Pose2D
+    {
+        double x;
+        double y;
+        double theta;
+    };
+
+} // namespace racecar_simulator
---
+++ ros_ws/src/f1tenth_simulator/include/f1tenth_simulator/st_kinematics.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "f1tenth_simulator/car_params.hpp"
+#include "f1tenth_simulator/car_state.hpp"
+
+namespace racecar_simulator
+{
+
+    class STKinematics
+    {
+
+        public:
+        static CarState update(const CarState start, double accel, double steer_angle_vel, CarParams p, double dt);
+
+        static CarState update_k(const CarState start, double accel, double steer_angle_vel, CarParams p, double dt);
+    };
+
+} // namespace racecar_simulator
